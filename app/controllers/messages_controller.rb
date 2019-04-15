2d8729cde025e90fc4e3a82fc12b786975454d9d
github username <github email address>
2019-04-15 19:56:27 -0400

    basic message controller methods

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,20 @@
 class MessagesController < ApplicationController
+    
+    def create
+        @message = Message.create(message_params)
+    end
+
+    def update
+
+    end
+
+    private
+
+    def message_params
+        params.require(:m).permit(:sender_id, :recipient_id, :sender_deleted, :recipient_deleted, :content, :subject)
+    end
+
+    def find_message
+        @message = Message.find(params[:id])
+    end
 end
