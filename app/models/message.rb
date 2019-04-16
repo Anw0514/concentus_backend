f78d8f5842e87a2e9f5473a67811de2f1e0d273c
github username <github email address>
2019-04-15 20:00:57 -0400

    message_serializer

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,14 @@
 class Message < ApplicationRecord
   belongs_to :sender, foreign_key: "sender_id", class_name: "User"
   belongs_to :recipient, foreign_key: "recipient_id", class_name: "User"
+
+  def message_serializer
+    {
+      sender_id: self.sender.id,
+      recipient_id: self.recipient.id,
+      subject: self.subject,
+      content: self.content,
+      date: self.created_at
+    }
+  end
 end
